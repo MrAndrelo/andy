9271950e1cf91e1b7eeafbd3310b157fd0bd4a13
MrAndrelo <mr.andrelo60@gmail.com>
2019-09-28 12:41:52 -0300

    se cambiaron estos elementos (iniciando el TPE WEB 2 - entrega 1) 	new file:   MVC/controlador.php 	new file:   MVC/modelo.php 	new file:   MVC/vista.php 	new file:   includes/arrays.php 	new file:   includes/footer.php 	new file:   includes/header.php 	new file:   includes/nav.php

---
+++ MVC/modelo.php
@@ -0,0 +1,52 @@
+<?php
+
+class ComidasModel {
+
+    function connect(){
+        $db = new PDO('mysql:host=localhost;'.'dbname=db_a_pedido;charset=utf8', 'root', '');
+        return $db;
+    }
+
+
+	public function getComidas(){
+        $db_connection = connect(); //abro coneccion
+        $query = $db_connection ->prepare( 'SELECT * FROM comidas'); //preparo la consulta
+        $ok = $query->execute(); //ejecuto consulta
+        if (!$ok) var_dump ($query -> errorinfo()); //chequeo ejecucion
+        $comidas = $query->fetchAll(PDO::FETCH_OBJ); //me da la respuesta
+        
+        return $comidas;
+    }	
+
+    function mostrarComidas() {
+        $comidas = getComidas();
+        $html = "<ul>";
+        foreach ($comidas as $comida) {
+            $html .="<li> {$comida->nombre}:{$comida->precio} </li>";
+        };
+        $html.="</ul>";
+
+        echo $html;
+    }
+}
+
+mostrarComidas();
+
+    function InsertarComida($nombre, $precio){
+
+        $sentencia = $this->db->prepare('INSERT INTO comidas(nombre, precio) VALUES(?,?,?,?)');
+        $sentencia->execute(array($nombre, $precio));
+    }
+
+    function EditarComida($id){
+        $sentencia =  $this->db->prepare('UPDATE comidas SET finalizada=1 WHERE id=?');
+        $sentencia->execute(array($id));
+    }
+
+    function BorrarComida($id){
+        $sentencia = $this->db->prepare('DELETE FROM comidas WHERE id=?');
+        $sentencia->execute(array($id));
+    }
+
+
+?>
